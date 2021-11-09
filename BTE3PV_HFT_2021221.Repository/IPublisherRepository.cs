6f61d80a0ba9dea2f366774540cf9262b8ff29bd
ÁdámBálint <adambalint9@stud.uni-obuda.hu>
2021-11-09 19:56:24 +0100

    Created  IPublisherRepository

--- BTE3PV_HFT_2021221.Repository/IPublisherRepository.cs
+++ BTE3PV_HFT_2021221.Repository/IPublisherRepository.cs
@@ -7,12 +7,13 @@ using BTE3PV_HFT_2021221.Models;
 
 namespace BTE3PV_HFT_2021221.Repository
 {
-    public interface IBookRepository
+    public interface IPublisherRepository
     {
-        void Create(Book book);
+        void Create(Publisher publisher);
         void Delete(int id);
-        Book Read(int id);
-        IQueryable<Book> ReadAll();
-        void Update(Book book);
+        Publisher Read(int id);
+        IQueryable<Publisher> ReadAll();
+        void Update(Publisher publisher);
+            
     }
 }
