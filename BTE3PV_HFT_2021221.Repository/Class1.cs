3b3a3f4951a0c90e4054ae161380e0642900b9f9
ÁdámBálint <adambalint9@stud.uni-obuda.hu>
2021-11-09 19:47:11 +0100

    created Bookrepository InterFace

--- BTE3PV_HFT_2021221.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BTE3PV_HFT_2021221.Repository
-{
-    public class Class1
-    {
-    }
-}
---
+++ BTE3PV_HFT_2021221.Repository/IBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BTE3PV_HFT_2021221.Models;
+
+namespace BTE3PV_HFT_2021221.Repository
+{
+    public interface IBookRepository
+    {
+        void Create(Book car);
+        void Delete(int id);
+        Book Read(int id);
+        IQueryable<Book> ReadAll();
+        void Update(Book car);
+    }
+}
