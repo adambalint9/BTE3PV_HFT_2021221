232ff717ffd426da770b19038e799d4859e619cd
ÁdámBálint <adambalint9@stud.uni-obuda.hu>
2021-11-09 19:37:30 +0100

    Creatin Authr Repo Interface

---
+++ BTE3PV_HFT_2021221.Repository/IAuthorRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BTE3PV_HFT_2021221.Models;
+
+namespace BTE3PV_HFT_2021221.Repository
+{
+    public interface IAuthorRepository
+    {
+        void Create(Author author);
+        void Delete(int id);
+        Author Read(int id);
+        IQueryable<Author> ReadAll();
+        void Update(Author author);
+
+
+
+    }
+}
