81caca6a51f1d87069ac53f3db6bf6fe2290e95e
ÁdámBálint <adambalint9@stud.uni-obuda.hu>
2021-11-09 21:12:12 +0100

    Creating AuthorRepository

---
+++ BTE3PV_HFT_2021221.Repository/AuthorRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BTE3PV_HFT_2021221.Data;
+using BTE3PV_HFT_2021221.Models;
+
+namespace BTE3PV_HFT_2021221.Repository
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+
+        LibraryDbContext db;
+
+        AuthorRepository(LibraryDbContext db)
+        {
+
+            this.db = db;
+        }
+
+        public void Create(Author author)
+        {
+            db.Authors.Add(author);
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            db.Remove(Read(id));
+            db.SaveChanges();
+        }
+
+        public Author Read(int id)
+        {
+            return db.Authors.FirstOrDefault(t => t.Id == id);
+        }
+
+        public IQueryable<Author> ReadAll()
+        {
+            return db.Authors;
+        }
+
+        public void Update(Author author)
+        {
+            var oldAuthor = Read(author.Id);
+            oldAuthor.AuthoreName = author.AuthoreName;
+            oldAuthor.Birthcountry = author.Birthcountry;
+            oldAuthor.BirthYear = author.BirthYear;
+            oldAuthor.Specialization = author.Specialization;
+            oldAuthor.WritingLanguage = author.WritingLanguage;
+            db.SaveChanges();
+
+
+        }
+    }
+}
