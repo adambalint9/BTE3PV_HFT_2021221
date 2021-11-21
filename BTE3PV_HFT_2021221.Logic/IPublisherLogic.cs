d9ba1f3cb8e0ce21d8411eb6d9b7574ad822203b
ÁdámBálint <adambalint9@stud.uni-obuda.hu>
2021-11-21 19:16:12 +0100

    Interface of Publisher logitc

---
+++ BTE3PV_HFT_2021221.Logic/IPublisherLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BTE3PV_HFT_2021221.Models;
+
+namespace BTE3PV_HFT_2021221.Logic
+{
+    interface IPublisherLogic
+    {
+        void Create(Publisher publisher);
+        void Delete(int id);
+        Publisher Read(int id);
+        IEnumerable<Publisher> ReadAll();
+        void Update(Publisher publisher);
+        IEnumerable<KeyValuePair<string, int>> CountBookByPublisher();
+        
+    }
+}
