855bfbc25e21ee98835d8c50e29b59536698964c
ÁdámBálint <adambalint9@stud.uni-obuda.hu>
2021-11-21 19:15:23 +0100

    Inter face of Author logic

---
+++ BTE3PV_HFT_2021221.Logic/IAuthorLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BTE3PV_HFT_2021221.Models;
+
+namespace BTE3PV_HFT_2021221.Logic
+{
+    interface IAuthorLogic
+    {
+        void Create(Author author);
+        void Delete(int id);
+        Author Read(int id);
+        IEnumerable<Author> ReadAll();
+        void Update(Author author);
+        IEnumerable<KeyValuePair<string, int>> CountBookByAuthor();
+       
+    }
+}
