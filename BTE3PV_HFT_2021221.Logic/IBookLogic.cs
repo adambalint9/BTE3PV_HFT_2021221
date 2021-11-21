35ca37c58b584cffdacfee1d6d4d3313e060f1d4
ÁdámBálint <adambalint9@stud.uni-obuda.hu>
2021-11-21 19:15:50 +0100

    Interface of Publisher logic

---
+++ BTE3PV_HFT_2021221.Logic/IBookLogic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BTE3PV_HFT_2021221.Models;
+
+namespace BTE3PV_HFT_2021221.Logic
+{
+    interface IBookLogic
+    {
+        void Create(Book book);
+        void Delete(int id);
+        Book Read(int id);
+        IEnumerable<Book> ReadAll();
+        void Update(Book book);       
+        IEnumerable<KeyValuePair<string, bool>> IsWritenOnTheWritersMothertounge();
+        IEnumerable<KeyValuePair<string, int>> CountBookByTopic();
+        IEnumerable<KeyValuePair<string, double>> AVGYeraPublished();
+    }
+}
