d3d4ad513b7434ff4c15852fa7fd34f6c8c5c37f
ÁdámBálint <adambalint9@stud.uni-obuda.hu>
2021-11-22 21:24:38 +0100

    5 NonCrud Nunitest finishd

---
+++ BTE3PV_HFT_2021221.Test/LibraryLogicTest.cs
@@ -0,0 +1,320 @@
+﻿using BTE3PV_HFT_2021221.Logic;
+using BTE3PV_HFT_2021221.Models;
+using BTE3PV_HFT_2021221.Repository;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BTE3PV_HFT_2021221.Test
+{
+    [TestFixture]
+    public class LibraryLogicTest
+    {
+        PublisherLogic pl;
+        AuthorLogic al;
+        BookLogic bl;
+        public LibraryLogicTest()
+        {
+            Mock<IBookRepository> mockbookrepo = new Mock<IBookRepository>();
+            Mock<IAuthorRepository> mockauthorrpo = new Mock<IAuthorRepository>();
+            Mock<IPublisherRepository> mockpulisherrepo = new Mock<IPublisherRepository>();
+
+
+            mockauthorrpo.Setup((t) => t.Create(It.IsAny<Author>()));
+            mockauthorrpo.Setup((t) => t.ReadAll()).Returns(
+               new List<Author>()
+                 {
+                     new Author()
+                     {
+                            AuthoreName = "Proba",
+                            Birthcountry = "England",
+                            BirthYear = 1995,
+                            WritingLanguage = "English",
+                            Specialization = "Math",
+                            Books= new List<Book>()
+                            {
+                                new Book()
+                                {
+                                     Language = "English",
+                                      Title = "Test1",
+                                     YearOfIssue = 2010,
+                                    Topic = "Math",
+                                    Lenght = 500,
+
+
+                                }
+                            }
+
+                     },
+                     new Author()
+                     {      AuthoreName = "Probalina",
+                             Birthcountry = "Hungary",
+                            BirthYear = 1970,
+                            WritingLanguage = "Hungarian",
+                            Specialization = "Biology",
+
+
+                            Books= new List<Book>()
+                            {
+                                new Book()
+                                {
+                                    Language = "English",
+                                    Title = "Test2",
+                                    YearOfIssue = 2010,
+                                    Topic = "Biology",
+                                    Lenght = 500,
+
+
+                                },
+
+                                 new Book()
+                                {
+                                    Language = "English",
+                                    Title = "Test3",
+                                    YearOfIssue = 2010,
+                                    Topic = "Biology",
+                                    Lenght = 500,
+
+
+                                },
+                                new Book()
+                                {
+                                    Language = "Hungarian",
+                                    Title = "Test4",
+                                    YearOfIssue = 2010,
+                                    Topic = "Phisics",
+                                    Lenght = 500,
+
+
+                                }
+
+
+
+                            }
+
+
+                     }
+
+
+
+               }.AsQueryable());
+
+
+
+
+            al = new AuthorLogic(mockauthorrpo.Object);
+
+            Author fakeauthor = new Author()
+            {
+                AuthoreName = "Proba",
+                Birthcountry = "England",
+                BirthYear = 1995,
+                WritingLanguage = "English",
+                Specialization = "Math"
+
+
+
+            };
+            Author fakeauthortwo = new Author()
+            {
+                AuthoreName = "Probalina",
+                Birthcountry = "Hungary",
+                BirthYear = 1970,
+                WritingLanguage = "Hungarian",
+                Specialization = "Biology"
+
+
+
+            };
+            Publisher fakepublisher = new Publisher()
+            {
+                PublisherName = "RealPublisher",
+                YearOfFundation = 1600,
+                Email = "Real@.com",
+                TelphoneNumber = 123,
+                Headquarters = "Budapest"
+
+
+            };
+            mockbookrepo.Setup((t) => t.Create(It.IsAny<Book>()));
+            mockbookrepo.Setup((t) => t.ReadAll()).Returns(
+           new List<Book>()
+           {
+               new Book()
+               {
+                 Language = "English",
+                  Title = "Test1",
+                 YearOfIssue = 2010,
+                Topic = "Math",
+                Lenght = 500,
+                Author = fakeauthor,
+                Publishers = fakepublisher
+
+               },
+
+               new Book()
+                    {
+                        Language = "English",
+                        Title = "Test2",
+                        YearOfIssue = 2010,
+                        Topic = "Biology",
+                        Lenght = 500,
+                        Author = fakeauthortwo,
+                        Publishers = fakepublisher
+
+                    },
+
+                 new Book()
+                {
+                    Language = "English",
+                    Title = "Test3",
+                    YearOfIssue = 2010,
+                    Topic = "Biology",
+                    Lenght = 500,
+                    Author = fakeauthortwo,
+                    Publishers = fakepublisher
+
+                },
+                new Book()
+                {
+                    Language = "Hungarian",
+                    Title = "Test4",
+                    YearOfIssue = 2010,
+                    Topic = "Phisics",
+                    Lenght = 500,
+                    Author = fakeauthortwo,
+                    Publishers = fakepublisher
+
+                }
+            }.AsQueryable());
+
+            bl = new BookLogic(mockbookrepo.Object);
+            mockpulisherrepo.Setup((t) => t.Create(It.IsAny<Publisher>()));
+            mockpulisherrepo.Setup((t) => t.ReadAll()).Returns(
+                new List<Publisher>()
+                {
+
+                    new Publisher()
+                    {
+                        PublisherName = "RealPublisher",
+                        YearOfFundation = 1600,
+                        Email = "Real@.com",
+                        TelphoneNumber = 123,
+                        Headquarters = "Budapest",
+                        Books= new List<Book>()
+                        {
+                            new Book()
+                           {
+                             Language = "English",
+                              Title = "Test1",
+                             YearOfIssue = 2010,
+                            Topic = "Math",
+                            Lenght = 500,
+                            Author = fakeauthor,
+                            Publishers = fakepublisher
+
+                           },
+
+                                new Book()
+                                {
+                                    Language = "English",
+                                    Title = "Test2",
+                                    YearOfIssue = 2010,
+                                    Topic = "Biology",
+                                    Lenght = 500,
+                                    Author = fakeauthortwo,
+                                    Publishers = fakepublisher
+
+                                },
+
+                             new Book()
+                            {
+                                Language = "English",
+                                Title = "Test3",
+                                YearOfIssue = 2010,
+                                Topic = "Biology",
+                                Lenght = 500,
+                                Author = fakeauthortwo,
+                                Publishers = fakepublisher
+
+                            },
+                            new Book()
+                            {
+                                Language = "Hungarian",
+                                Title = "Test4",
+                                YearOfIssue = 2010,
+                                Topic = "Phisics",
+                                Lenght = 500,
+                                Author = fakeauthortwo,
+                                Publishers = fakepublisher
+
+                            }
+
+                        }
+
+                    }
+
+                }.AsQueryable());
+
+            pl = new PublisherLogic(mockpulisherrepo.Object);
+
+
+        }
+        [Test]
+        public void CountByTopicTest()
+        {
+            var result = bl.CountBookByTopic().ToArray();
+
+            //Assert.That(result[0], Is.EqualTo(new KeyValuePair<string, int>("Proba", 1)));
+            Assert.That(result[1], Is.EqualTo(new KeyValuePair<string, int>("Probalina", 2)));
+
+
+        }
+        [Test]
+        public void AVG()
+        {
+            var result = bl.AVGYeraPublished().ToArray();
+
+
+            Assert.That(result[0], Is.EqualTo(new KeyValuePair<string, double>("RealPublisher", 2010)));
+
+
+        }
+
+        [Test]
+        public void IsWritenOnTheWritersMothertounge()
+        {
+            var result = bl.IsWritenOnTheWritersMothertounge().ToArray();
+
+
+            Assert.That(result[2], Is.EqualTo(new KeyValuePair<string, bool>("Test3", false)));
+           // Assert.That(result[0], Is.EqualTo(new KeyValuePair<string, bool>("Test1", true)));
+        }
+
+        [Test]
+        public void CountBookByAuthor()
+        {
+
+            var results = al.CountBookByAuthor().ToArray();
+
+            Assert.That(results[1], Is.EqualTo(new KeyValuePair<string, int>("Probalina", 3)));
+
+
+        }
+        [Test]
+        public void CountBookByPublisher()
+        {
+            var results = pl.CountBookByPublisher().ToArray();
+
+            Assert.That(results[0], Is.EqualTo(new KeyValuePair<string, int>("RealPublisher", 4)));
+
+
+
+
+        }
+
+
+
+    }
+}
